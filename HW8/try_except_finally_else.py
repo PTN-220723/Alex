14399a18741609eebc3fe472153e627a59edc086
a-ptrv <a.ptrv@yandex.ru>
2023-09-15 18:03:44 +0500

    try_except_finally_else.py up to 3)

--- HW8/try_except_finally_else.py
+++ HW8/try_except_finally_else.py
@@ -1,4 +1,4 @@
-1)
+#1)
 while True:
     num1 = input('Введите первое число: ')
     num2 = input('Введите второе число: ')
@@ -15,7 +15,7 @@ while True:
             print(f'Второе значение: {num2}')
             print(f'Результат: {num1 + num2}')
 
-2)
+#2)
 list1 = [1, 'a', 3, 'b', 5, '6', 7, '8', 9, 'c']
 num1 = []
 str1 = []
@@ -30,3 +30,21 @@ for i in list1:
 print(list1)
 print(str1)
 print(num1)
+
+#3)
+list1 = [1, 'a', 3, 'b', 5, '6', 7, '8', 9, 'c']
+num1 = []
+str1 = []
+for i in list1:
+    test = 0
+    try:
+        test += i
+    except:
+        str1.append(i)
+        print(f'В список чисел добавлен элемент {i} : {str1}')
+    else:
+        num1.append(i)
+        print(f'В список строк добавлен элемент {i} : {num1}')
+print(list1)
+print(f'Строки {str1}')
+print(f'Числа {num1}')
