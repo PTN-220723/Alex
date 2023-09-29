ec036b076a77c0897374f0f24901a62fca7492f8
a-ptrv <a.ptrv@yandex.ru>
2023-09-29 23:29:56 +0500

    'task-2'

--- HW10/zip_map_filter_lambda/task-2.py
+++ HW10/zip_map_filter_lambda/task-2.py
@@ -1,6 +1,7 @@
 def more_than_75(var: list) -> list:
+    #return var > 75
     list_75 = []
-    for score in scores:
+    for score in var:
         if score > 75:
             list_75.append(score)
 
@@ -12,3 +13,5 @@ list1 = more_than_75(scores)
 print(list1)
 list2 = list(filter(lambda score: score > 75, scores))
 print(list2)
+# list3 = list(filter(more_than_75, scores))
+# print(list3)
